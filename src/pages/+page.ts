39e6f4968b8df6a29eafe7f0cc6bdc897e836f70
pikoUsername <galymzhan.amantaj@gmail.com>
2025-06-01 10:41:24 +0500

    Yes

--- src/pages/+page.ts
+++ src/pages/+page.ts
@@ -1,16 +1,4 @@
-import { goto } from '$app/navigation';
-import { CurrentUser } from '$lib/entities/user/model/modal';
-import { UserEntityType } from 'flsurf-client';
 import type { PageLoad } from './$types';
-import { get } from 'svelte/store';
 
 export const load: PageLoad = async () => {
-    let currentUser = get(CurrentUser)
-    
-    if (currentUser === undefined)
-        return 
-
-    if (currentUser.type == UserEntityType.NonUser) { 
-        goto("/auth/complete-profile")
-    } 
 };
